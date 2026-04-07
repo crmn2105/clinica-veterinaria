3ff580c3d6ef35b55531d2738c13677c6f92397f
crmn2105 <crmn2105@gmail.com>
2026-04-07 10:36:25 +0200

    add vercel api

---
+++ api/index.py
@@ -0,0 +1,23 @@
+from http.server import BaseHTTPRequestHandler
+import json
+import os
+from openai import OpenAI
+
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "user", "content": "Hola"}
+            ]
+        )
+
+        self.send_response(200)
+        self.send_header('Content-type','application/json')
+        self.end_headers()
+
+        self.wfile.write(json.dumps({
+            "respuesta": response.choices[0].message.content
+        }).encode())
